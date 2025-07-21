a8afd409306b26cbd3d47b3fccbccfcf5b191bf4
Natalia <nataliarm03@gmail.com>
2025-07-21 12:23:17 -0600

    Subiendo proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Ostara</h1>
+      <p>
+        Aquí irá el homepage
+      </p>
+    </>
+  )
+}
+
+export default App
